3b425accb6716197f54bcdabbad0658ad6422e0a
ADITHYAN <adithynps3@gmial.com>
2019-08-18 11:59:32 +0530

    Updates

---
+++ precache-manifest.cfd156bb32446a4a3fd95642a21d67dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9f8356dcd1a5eb5c795",
+    "url": "/static/css/main.13502cd8.chunk.css"
+  },
+  {
+    "revision": "a9f8356dcd1a5eb5c795",
+    "url": "/static/js/main.a9f8356d.chunk.js"
+  },
+  {
+    "revision": "7f32f094c43b737e0e2c",
+    "url": "/static/css/1.bb940768.chunk.css"
+  },
+  {
+    "revision": "7f32f094c43b737e0e2c",
+    "url": "/static/js/1.7f32f094.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "edb2a9eb112e2b1fe7f3d32dd550d004",
+    "url": "/static/media/adithyan.edb2a9eb.jpg"
+  },
+  {
+    "revision": "688d3264ca8a853c74993db940759eea",
+    "url": "/index.html"
+  }
+];
